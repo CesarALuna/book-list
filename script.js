675981f4836e08db2819821d82624aab12a52c26
CesarALuna <CesLun@gmail.com>
2023-04-18 06:07:25 -0700

    update to class-based components

--- script.js
+++ script.js
@@ -1,43 +1,88 @@
-/* eslint-disable no-shadow */
-/* eslint-disable no-plusplus */
+/* eslint-disable max-classes-per-file */
+// function Book(title, author, isbn, hasRead) {
+//   this.title = title;
+//   this.author = author;
+//   this.isbn = isbn;
+//   this.hasRead = hasRead;
+// }
 
-const library = [];
-
-function Book(title, author, isbn, hasRead) {
-  this.title = title;
-  this.author = author;
-  this.isbn = isbn;
-  this.hasRead = hasRead;
-}
-
-function addBookToLibrary(title, author, isbn, hasRead) {
-  const newBook = new Book(title, author, isbn, hasRead);
-  library.push(newBook);
-}
-
-function displayBooks(library) {
-  const list = document.querySelector("#book-list");
-  let books = "";
-
-  // loop through each book in the library
-  for (let i = 0; i < library.length; i++) {
-    const book = library[i];
-    const readStatus = book.hasRead ? "fa-square-check" : "";
-    books += `
-        <tr>
-        <td>${book.title}</td>
-        <td>${book.author}</td>
-        <td>${book.isbn}</td>
-        <td><input type="checkbox" onchange="toggleReadStatus(library[${i}], this)"></td>
-        <td><a href='#'><i class="fa-regular fa-trash-can" onclick="removeBook(${i})" ></i></a></td>
-        </tr>
-    `;
+class Book {
+  constructor(title, author, isbn, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.hasRead = hasRead;
   }
-  list.innerHTML = books;
 }
+
+// function addBookToLibrary(title, author, isbn, hasRead) {
+//   const newBook = new Book(title, author, isbn, hasRead);
+//   library.push(newBook);
+// }
+
+// function displayBooks(library) {
+//   const list = document.querySelector("#book-list");
+//   let books = "";
+
+//   // loop through each book in the library
+//   for (let i = 0; i < library.length; i++) {
+//     const book = library[i];
+//     const readStatus = book.hasRead ? "fa-square-check" : "";
+//     books += `
+//         <tr>
+//         <td>${book.title}</td>
+//         <td>${book.author}</td>
+//         <td>${book.isbn}</td>
+//         <td><input type="checkbox" onchange="toggleReadStatus(library[${i}], this)"></td>
+//         <td><a href='#'><i class="fa-regular fa-trash-can" onclick="removeBook(${i})" ></i></a></td>
+//         </tr>
+//     `;
+//   }
+//   list.innerHTML = books;
+// }
 
 // invoke function
-displayBooks(library);
+
+class Library {
+  constructor() {
+    this.books = [];
+  }
+
+  addBook(title, author, isbn, hasRead) {
+    const newBook = new Book(title, author, isbn, hasRead);
+    this.books.push(newBook);
+  }
+
+  removeBook(index) {
+    this.books.splice(index, 1);
+    this.displayBooks();
+  }
+
+  displayBooks() {
+    const list = document.querySelector("#book-list");
+    let books = "";
+
+    // loop through each book in the library
+    for (let i = 0; i < this.books.length; i++) {
+      const book = this.books[i];
+      const readStatus = book.hasRead ? "fa-square-check" : "";
+      books += `
+                <tr>
+          <td>${book.title}</td>
+          <td>${book.author}</td>
+          <td>${book.isbn}</td>
+          <td><input type="checkbox" onchange="toggleReadStatus(library.books[${i}], this)"></td>
+          <td><a href='#'><i class="fa-regular fa-trash-can" onclick="library.removeBook(${i})"></i></a></td>
+          </tr>
+      `;
+    }
+    list.innerHTML = books;
+  }
+}
+
+const library = new Library();
+
+library.displayBooks();
 
 // clear fields
 function clearFields() {
@@ -46,29 +91,21 @@ function clearFields() {
   document.querySelector("#isbn").value = "";
 }
 
-// add a new book
 document.querySelector("#book-form").addEventListener("submit", (e) => {
   e.preventDefault();
 
-  // cache form values
   const title = document.querySelector("#title").value;
   const author = document.querySelector("#author").value;
   const isbn = document.querySelector("#isbn").value;
 
-  // validate
   if (title === "" || author === "" || isbn === "") {
     alert("Please complete all fields");
   } else {
-    addBookToLibrary(title, author, isbn);
+    library.addBook(title, author, isbn);
   }
-  displayBooks(library);
+  library.displayBooks(library);
   clearFields();
 });
-
-function removeBook(index) {
-  library.splice(index, 1);
-  displayBooks(library);
-}
 
 function toggleReadStatus(book) {
   book.hasRead = !book.hasRead;
